f6831bf13fe622915d804db0231ed90374177bc7
Matt Cook <matt@lookitscook.com>
2014-06-25 19:30:31 -0500

    added gamma gallery functionality

---
+++ js/gallery.js
@@ -0,0 +1,47 @@
+$(function() {
+
+    var GammaSettings = {
+            // order is important!
+            viewport : [ {
+                width : 1200,
+                columns : 4
+            }, {
+                width : 900,
+                columns : 4
+            }, {
+                width : 500,
+                columns : 3
+            }, { 
+                width : 320,
+                columns : 2
+            }, { 
+                width : 0,
+                columns : 1
+            } ]
+    };
+
+    Gamma.init( GammaSettings, function(){ /* callback */} );
+  
+  
+    //hack to reload window since Gamma Gallery breaks on page resize
+    var rtime = new Date(1, 1, 2000, 12,00,00);
+    var timeout = false;
+    var delta = 200;
+    $(window).resize(function() {
+        rtime = new Date();
+        if (timeout === false) {
+            timeout = true;
+            setTimeout(resizeend, delta);
+        }
+    });
+
+    function resizeend() {
+        if (new Date() - rtime < delta) {
+            setTimeout(resizeend, delta);
+        } else {
+            timeout = false;
+            location.reload();
+        }               
+    }
+
+});
